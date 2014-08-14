4fb2bf935a6ab2f23fd191020fa9e115e3fa21bd
Juan <jsamesub@gmail.com>
2014-08-14 18:03:05 -0500

    Fixed project files

--- app/controllers/gamblers.js
+++ app/controllers/gamblers.js
@@ -1,12 +1,9 @@
-
 'use strict';
 
-var Gambler =  require('../models/gambler');
 
-exports.init = function(req, res){
-  Gambler.all(function(err,gamblers){
+exports.index = function(req, res){
+  Gambler.all(function(err, gamblers){
     console.log(gamblers);
-    res.render('gamblers/index', {gamblers:gamblers});
-  });
+   res.render('gamblers/index', {gamblers: gamblers});
+  }); 
 };
-
